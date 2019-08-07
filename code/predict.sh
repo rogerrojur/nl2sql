d72b45de2c46bdc90f8bcb411a04ddae088dd536
haoojin <tojinhao@gmail.com>
2019-08-07 18:14:09 +0800

    修改生成结果的batch size参数为32，同时修改*.sh文件执行权限

--- code/predict.sh
+++ code/predict.sh
@@ -1,1 +1,1 @@
-python generate_result.py --seed 1 --bS 4 --accumulate_gradients 2 --bert_type_abb zhS --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_leng 400 --EG
+python generate_result.py --seed 1 --bS 32 --accumulate_gradients 2 --bert_type_abb zhS --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_leng 400 --EG
